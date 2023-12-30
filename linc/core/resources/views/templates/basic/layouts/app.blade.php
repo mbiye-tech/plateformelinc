5c9de646672193863e9bea314e53c97982ba548e
merdikiba <merdikibasonga@gmail.com>
2023-12-30 08:56:55 +0100

    all

--- linc/core/resources/views/templates/basic/layouts/app.blade.php
+++ linc/core/resources/views/templates/basic/layouts/app.blade.php
@@ -9,6 +9,7 @@
     @include('partials.seo')
     <link rel="icon" href="{{ getImage(getFilePath('logoIcon') . '/favicon.png') }}" sizes="16x16" type="image/png" />
     <link rel="stylesheet" href="{{ asset('assets/global/css/bootstrap.min.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('assets/global/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/global/css/line-awesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/global/css/slick.css') }}">
